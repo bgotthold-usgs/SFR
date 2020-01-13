37dea013413b16cb2c4c4aafd56dd31dc49c9e0a
dawsons <dawsons@contractor.usgs.gov>
2020-01-13 12:11:02 -0700

    BCB-1309.  Add County processing

---
+++ county/bis_pipeline.py
@@ -0,0 +1,42 @@
+import os
+import json
+from county.county import processBuffer
+from resources.shapefile_helper import getGeoJsonFromShapefile
+
+json_schema = None
+try:
+    with open(os.path.join(os.path.dirname(__file__), './SFR_schema.json'), 'r') as json_schema_file:
+        json_schema = json.load(json_schema_file)
+except FileNotFoundError as e:
+    pass
+
+
+def process_1(
+        path,
+        ch_ledger,
+        send_final_result,
+        send_to_send_to_stage,
+        previous_stage_result,
+):
+    count = 0
+    buffer = getGeoJsonFromShapefile(path)
+    for b in buffer:
+        data = processBuffer(b, change_log_function=ch_ledger.log_change_event)
+        result = get_json_doc(data)
+        send_final_result(result)
+        count += 1
+
+    return count
+
+
+def get_json_doc(data):
+    return {
+        "data": {
+            "feature_id": data["properties"]["feature_id"],
+            "feature_name": data["properties"]["feature_name"],
+            "feature_description": data["properties"]["feature_description"],
+            "feature_class": data["properties"]["feature_class"],
+        },
+        "row_id": data["properties"]["feature_id"],
+        "geometry":  data["geometry"]
+    }
